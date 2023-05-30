242a1b87b61149a7e02db7916a42203b129c98c7
Kara Anderson <kanderson8585@gmail.com>
2023-05-30 10:43:22 -0600

    Setup basic component structure

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,6 +4,14 @@ import { useState } from 'react';
 function App() {
   const [search, setSearch] = useState('');
 
+  const submitSearch = (e) => {
+    e.preventDefault();
+
+    fetch('https://api.github.com/orgs/Netflix/repos')
+      .then(response => response.json())
+      .then(data => console.log(data))
+  }
+
   return (
     <div>
       <h1>Search for a GitHub organization</h1>
@@ -24,7 +32,7 @@ function App() {
                   placeholder="Search for a GitHub Organization"
                 />
             </div>
-            <button type="submit" className="btn btn-primary">
+            <button type="submit" className="btn btn-primary" onClick={(e) => submitSearch(e)}>
               Search
             </button>
           </form>
---
+++ src/components/RepoCard/RepoCard.js
@@ -0,0 +1,10 @@
+const RepoCard = () => {
+ 
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default RepoCard
