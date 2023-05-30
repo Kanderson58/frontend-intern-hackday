c37b744cd5b730e91adeda36e4b941a9d53193f5
Kara Anderson <kanderson8585@gmail.com>
2023-05-30 13:25:11 -0600

    Fix styling

--- src/components/RepoCard/RepoCard.js
+++ src/components/RepoCard/RepoCard.js
@@ -10,7 +10,7 @@ const RepoCard = ({repo, getSingleRepo}) => {
         <li>{repo.description ? 'Description: ' + repo.description : 'Sorry, no description available'}</li>
         <li>{repo.stargazers_count ? 'Number of stars: ' + repo.stargazers_count : 'Sorry, unkown number of stars'}</li>
         <li>{repo.forks_count ? 'Number of forks: ' + repo.forks_count : 'Sorry, unknown number of forks'}</li>
-        <li>{repo.created_at ? 'Created on: ' + repo.created_at : 'Sorry, unknown creation date'}</li>
+        <li>{repo.created_at ? 'Created on: ' + repo.created_at.substring(0, 10) : 'Sorry, unknown creation date'}</li>
       </ul>
       <button id={`${repo.commits_url.substring(0, repo.commits_url.length - 6)}`} onClick={(e) => {getSingleRepo(e.target.id)}}>See Commits for this Repository</button>
     </div>
--- src/components/RepoCommits/RepoCommits.js
+++ src/components/RepoCommits/RepoCommits.js
@@ -6,7 +6,7 @@ const RepoCommits = ({commit}) => {
     <ul className='commit' tabIndex='0'>
       <li>Commit made by: {commit.commit.author.name}</li>
       <li className='message'>Commit message: {commit.commit.message}</li>
-      <li>Commit date: {commit.commit.author.date}</li>
+      <li>Commit date: {commit.commit.author.date.substring(0, 10)}</li>
       <li>Commit hash: {commit.sha}</li>
     </ul>
   )
